29c0c8bd769f38efea788a248aac0fbf671d8445
glennbech <glenn.bech@gmail.com>
2025-11-13 07:29:21 +0100

    prepared for lab

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket         = "glenn-pracice-state"
-        key            = "website/terraform.tfstate"
-        region         = "eu-west-1"
-        dynamodb_table = "terraform-state-locks"
-        encrypt        = true
-    }
-}
